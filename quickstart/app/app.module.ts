e87e4acc5d35a8ad85c92d21e53b2b352c580aa5
Edward Erik Schmoll <erikschmoll@gmail.com>
2016-08-23 01:18:14 -0300

    carga incial

---
+++ quickstart/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { routing }        from './app.routing';
+import { DashboardComponent }      from './dashboard.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
