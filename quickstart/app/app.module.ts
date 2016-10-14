ca33229653e5d00b1e3a4020a9892002966daf33
Edward Schmoll <eschmoll@sofrecom.com.ar>
2016-10-14 11:45:51 -0300

    agrego data table net

--- quickstart/app/app.module.ts
+++ quickstart/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { AppComponent }  from './component/main/app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { routing }        from './app.routing';
--- quickstart/app/component/solicitude/solicitude.component.ts
+++ quickstart/app/component/solicitude/solicitude.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+
+declare var $:any;
+
 
 @Component({
     selector: 'solicitude',
-    templateUrl: 'app/component/solicitude/solicitude-form.html'
+    templateUrl: 'app/component/solicitude/solicitude-list.html'
 })
+export class SolicitudeComponent{
+    title :string;
+    solicitudes :[any];
+    constructor(){
+        this.title = "Solicitudes"
+        this.solicitudes = [{name: "doli B"}]
+        
+    }
 
-export class SolicitudeComponent{}
+    ngAfterViewInit() {
+       $('#solicitude_dt').DataTable({
+           data: this.solicitudes,
+           columns: [
+            { "data": "name", "title": "Pepe" }
+        ]});
+    }
+
+}
